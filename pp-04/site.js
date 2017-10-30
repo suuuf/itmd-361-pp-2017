e1cf0a1c2313193d65abc570aa16f7d118329afb
Sufyan Menk <smenk@hawk.iit.edu>
2017-10-29 19:21:28 -0500

    finished #3

--- pp-04/site.js
+++ pp-04/site.js
@@ -18,12 +18,16 @@ console.log('The value of x is:', x, 'It should be 5.');
 //  function can be accessed via the `window` global object,
 //  like `window.x`):
 
+var anon = function double(num) {
+  return (num*2);
+}
+console.log('Anonymous double function with num:6 result:' + anon(6));
 
 //  3. Correct this function so that there is no i variable in
 //  the global scope:
 
 function arrayEach(array, func) {
-  for (i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     func(array[i]);
   }
 }
