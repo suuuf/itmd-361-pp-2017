6bf644fedf52752c6a86e0b67c66493f57b58a06
Sufyan Menk <smenk@hawk.iit.edu>
2017-11-13 17:37:05 -0600

    added hide class to ring doorbell

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,3 +2,11 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+$.noConflict();
+
+(function($) {
+  $('html').removeClass('nojs').addClass('js');
+  $('#doorbell').addClass('hide');
+})(jQuery);
+
