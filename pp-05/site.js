0c530733b41e1c25310860e23d1ae6565096b426
Sufyan Menk <smenk@hawk.iit.edu>
2017-11-13 20:26:32 -0600

    added style for button

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,16 +1,21 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+
 
 $.noConflict();
 
 (function($) {
-  
+  var html = "  <label for='doorbell'>Click to Ring the Doorbell: </label><input type='button' id='doorbell' value='Play'>";
   $('html').removeClass('nojs').addClass('js');
   $('#doorbell').remove();
-  var input = "<input type='button' id='doorbell' value='Play/Pause'>";
-  $("body").append(input);
+  $("body").append(html);
+  
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+  
+  $('#doorbell').on('click', function() {
+    ringDoorbell();
+  });
   
 })(jQuery);
 
